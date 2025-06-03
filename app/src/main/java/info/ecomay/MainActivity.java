a4e47c02ecfc9f3208ee909f16edc43e32219484
prernagoswami <prernag.sky@gmail.com>
2025-06-03 22:43:59 +0530

    Commit 3-6-2025 intent

--- app/src/main/java/info/ecomay/MainActivity.java
+++ app/src/main/java/info/ecomay/MainActivity.java
@@ -1,6 +1,12 @@
 package info.ecomay;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +14,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
+
+    Button login,createAccount;
+    EditText email,password;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,54 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        login = findViewById(R.id.main_login);
+        createAccount = findViewById(R.id.main_create_account);
+
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        email = findViewById(R.id.main_email);
+        password = findViewById(R.id.main_password);
+
+        login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if (email.getText().toString().trim().equals("")) {
+                    email.setError("Email ID Required");
+                } else if (!email.getText().toString().trim().matches(emailPattern)) {
+                    email.setError("Valid Email ID Required");
+                    
+                } else if (password.getText().toString().trim().equals("")) {
+                    password.setError("Password Required");
+
+                } else if (password.getText().toString().trim().length()<6) {
+                    password.setError("Minimum 6 Character Password Required");
+                    
+                } else {
+                    if (email.getText().toString().trim().equalsIgnoreCase("admin@gamil.com") && password.getText().toString().trim().equalsIgnoreCase("Admin@123") ) {
+                        System.out.println("Login Successfully");
+                        Log.d("RESPONSE", "Login Successfully");
+                        Log.e("RESPONSE", "Login Successfully");
+                        Toast.makeText(MainActivity.this, "Login Successfully✅", Toast.LENGTH_LONG).show();
+                        Snackbar.make(view, "Login Successfully✅", Snackbar.LENGTH_SHORT).show();
+
+                        Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
+                        startActivity(intent);
+
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this, "Login Unsuccessfully❌", Toast.LENGTH_LONG).show();
+                        Snackbar.make(view, "Login Unsuccessfully❌", Snackbar.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
     }
 }
